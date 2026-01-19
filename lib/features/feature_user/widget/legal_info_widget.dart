d7999e9c39ce1f9ca16d472f744b16cbc59238ea
Bak Nguen <sangsangden@gmail.com>
2026-01-19 12:26:12 +0700

    privacy + term service ready

--- lib/features/feature_user/widget/legal_info_widget.dart
+++ lib/features/feature_user/widget/legal_info_widget.dart
@@ -1,4 +1,6 @@
+import 'package:baby_look/core/app_constant/app_constant.dart';
 import 'package:baby_look/core/app_icon/app_icon.dart';
+import 'package:baby_look/core/utils/lauch_to_url.dart';
 import 'package:baby_look/shared/custom_listile.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -13,8 +15,16 @@ class LegalInfoWidget extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text('Legal', style: theme.textTheme.titleMedium),
-        CustomListile(title: 'Terms Of Service', icon: AppIcon.termServiceIcon),
-        CustomListile(title: 'Pravicy Policy', icon: AppIcon.pravicyPolicyIcon),
+        CustomListile(
+          title: 'Terms Of Service',
+          icon: AppIcon.termServiceIcon,
+          onTap: () async {
+            await lauch_to_url(url: AppConstant.termServiceUrl);
+          },
+        ),
+        CustomListile(title: 'Pravicy Policy', icon: AppIcon.pravicyPolicyIcon,  onTap: () async {
+            await lauch_to_url(url: AppConstant.privacyPolicyUrl);
+          },),
         CustomListile(
           title: 'About AI BabyLook',
           icon: AppIcon.infoIcon,
