31a4fe6c81515f16f0bbcd9216be91c88fb0b25c
Bak Nguen <sangsangden@gmail.com>
2026-01-23 15:18:16 +0700

    empty case

--- lib/features/feature_dashboard/presentation/widget/recent_predictions_widget.dart
+++ lib/features/feature_dashboard/presentation/widget/recent_predictions_widget.dart
@@ -1,12 +1,12 @@
 import 'package:baby_look/core/app_constant/app_constant.dart';
 import 'package:baby_look/core/app_text/app_text.dart';
 import 'package:baby_look/features/feature_gallery/bloc/predictions_bloc.dart';
+import 'package:baby_look/shared/empty_widget.dart';
 import 'package:baby_look/shared/generated_card_widget.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:skeletonizer/skeletonizer.dart';
 
 class RecentPredictionsWidget extends StatelessWidget {
   const RecentPredictionsWidget({super.key});
@@ -20,7 +20,10 @@ class RecentPredictionsWidget extends StatelessWidget {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(context.tr(AppText.recent_predictions), style: theme.textTheme.titleMedium),
+            Text(
+              context.tr(AppText.recent_predictions),
+              style: theme.textTheme.titleMedium,
+            ),
             TextButton(
               onPressed: () {
                 context.go('/gallery');
@@ -45,6 +48,13 @@ class RecentPredictionsWidget extends StatelessWidget {
     return BlocBuilder<PredictionsBloc, PredictionsBlocState>(
       builder: (context, state) {
         if (state is PredictionsBlocState_loaded) {
+          ///
+          /// EMPTY CASE
+          ///
+          if (state.predictionList.isEmpty) {
+            return Center(child: EmptyWidget());
+          }
+
           return GridView.builder(
             // only show latest 4
             itemCount: state.predictionList.length <= 4
@@ -63,40 +73,16 @@ class RecentPredictionsWidget extends StatelessWidget {
               final prediction = state.predictionList[index];
               return GeneratedCardWidget(
                 prediction: prediction,
-                onCardTap: () => context.go(
-                  '/gallery/prediction_detail',
-                  extra: prediction,
-                ),
+                onCardTap: () =>
+                    context.go('/gallery/prediction_detail', extra: prediction),
               );
             },
           );
         } else {
-
           ///
-          /// LOADING 
+          /// LOADING
           ///
-          return Skeletonizer(
-            enabled: false,
-            child: GridView.builder(
-              // only show latest 4
-              itemCount:  4,
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                mainAxisSpacing: AppConstant.appPadding,
-                crossAxisSpacing: AppConstant.appPadding,
-                crossAxisCount: 2,
-                childAspectRatio: 3 / 4,
-              ),
-              itemBuilder: (context, index) {
-              
-                return GeneratedCardWidget(
-                
-                );
-              },
-            ),
-          );
+          return EmptyWidget();
         }
       },
     );
